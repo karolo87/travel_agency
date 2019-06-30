89983c38d6e47e547f08639c7f14cf0814ae4289
Karol <ostr.karol@gmail.com>
2019-06-30 13:54:03 +0200

    initial commit

---
+++ src/main/java/com/travelagency/demo/TravelagencyApplication.java
@@ -0,0 +1,13 @@
+package com.travelagency.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelagencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelagencyApplication.class, args);
+	}
+
+}
